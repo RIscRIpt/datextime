921dfcd3127c4b6066cd4e7c3d8a06c9e95c60f5
RIscRIpt <richard@riscript.com>
2019-12-23 19:34:47 +0200

    improve recognition

--- ner.js
+++ ner.js
@@ -1,18 +1,49 @@
+function entriesOverlap(e1, e2) {
+    return e1.start <= e2.end && e2.start <= e1.end;
+}
+
 class NER {
     constructor () {
         this.dateTimeRecognizer = new microsoftRecognizersTextDateTime.DateTimeRecognizer(microsoftRecognizersText.Culture.English);
+        this.sequenceRecognizer = new microsoftRecognizersTextSequence.SequenceRecognizer(microsoftRecognizersText.Culture.English);
+
         this.dateTimeModel = this.dateTimeRecognizer.getDateTimeModel();
+        this.ipModel = this.sequenceRecognizer.getIpAddressModel();
+        this.phoneModel = this.sequenceRecognizer.getPhoneNumberModel();
+    }
+
+    filterEntry(query, entry) {
+        if (query.substring(entry.start, entry.end + 1) === "may") {
+            return false;
+        }
+
+        let phones = this.phoneModel.parse(query);
+        for (let phone of phones) {
+            if (phone.resolution.score < 0.5) {
+                continue;
+            }
+            if (entriesOverlap(entry, phone)) {
+                return false;
+            }
+        }
+
+        let ips = this.ipModel.parse(query);
+        for (let ip of ips) {
+            if (ip.resolution.score < 0.5) {
+                continue;
+            }
+            if (entriesOverlap(entry, ip)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     parse(query, referenceDate) {
         try {
             let results = this.dateTimeModel.parse(query, referenceDate);
-            return results.filter(entity => {
-                if (query.substring(entity.start, entity.end + 1) === "may") {
-                    return false;
-                }
-                return true;
-            });
+            return results.filter(entry => this.filterEntry(query, entry));
         } catch (error) {
             return [];
         }
